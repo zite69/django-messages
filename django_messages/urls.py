ef84f57274104707aadb38e8f2a1759dc61c6aa6
Arun Kumar <arunkakorp@gmail.com>
2024-10-23 04:07:47 +0530

    Updated to django.urls re_path
    to silence the warnings on Django 4.x

--- django_messages/urls.py
+++ django_messages/urls.py
@@ -1,16 +1,16 @@
 from django.views.generic import RedirectView
-from django.urls import include, path
+from django.urls import include, re_path
 from django_messages.views import *
 
 urlpatterns = [
-    path('', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
-    path('inbox/', inbox, name='messages_inbox'),
-    path('outbox/', outbox, name='messages_outbox'),
-    path('compose/', compose, name='messages_compose'),
-    path('compose/(?P<recipient>[\w.@+-]+)/', compose, name='messages_compose_to'),
-    path('reply/(?P<message_id>[\d]+)/', reply, name='messages_reply'),
-    path('view/(?P<message_id>[\d]+)/', view, name='messages_detail'),
-    path('delete/(?P<message_id>[\d]+)/', delete, name='messages_delete'),
-    path('undelete/(?P<message_id>[\d]+)/', undelete, name='messages_undelete'),
-    path('trash/$', trash, name='messages_trash'),
+    re_path('', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
+    re_path('inbox/', inbox, name='messages_inbox'),
+    re_path('outbox/', outbox, name='messages_outbox'),
+    re_path('compose/', compose, name='messages_compose'),
+    re_path('compose/(?P<recipient>[\w.@+-]+)/', compose, name='messages_compose_to'),
+    re_path('reply/(?P<message_id>[\d]+)/', reply, name='messages_reply'),
+    re_path('view/(?P<message_id>[\d]+)/', view, name='messages_detail'),
+    re_path('delete/(?P<message_id>[\d]+)/', delete, name='messages_delete'),
+    re_path('undelete/(?P<message_id>[\d]+)/', undelete, name='messages_undelete'),
+    re_path('trash/$', trash, name='messages_trash'),
 ]
